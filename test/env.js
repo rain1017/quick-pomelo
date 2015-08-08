96ecb711a1a371c3fcfefc4bfe3254d5484f729b
Yu Xia <rain1017@gmail.com>
2015-08-08 11:37:30 +0800

    update test/env.js

--- test/env.js
+++ test/env.js
@@ -23,11 +23,9 @@ var logger = quick.logger.getLogger('test', __filename);
 var P = quick.Promise;
 P.longStackTraces();
 
-var memdbClusterPath = '/usr/local/bin/memdbcluster';
-
 var execMemdbClusterSync = function(cmd){
     var configPath = path.join(__dirname, 'memdb.conf.js');
-    var output = child_process.execFileSync(process.execPath, [memdbClusterPath, cmd, '--conf=' + configPath]);
+    var output = child_process.execFileSync('memdbcluster', [cmd, '--conf=' + configPath]);
     logger.info(output.toString());
 };
 
